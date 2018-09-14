d3ca5039fe402251c29a8d54451094b65c197204
Nick <nhorton04@gmail.com>
2018-09-13 17:01:02 -0700

    add login / logout /signup functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    debugger
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    session[:banana] = 'banana'
+  end
 end
